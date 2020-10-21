57cb082f6b625a8d14e97bad9c05e6f5bd40b5bd
Lloyd <54277450+pypinkfloydfan@users.noreply.github.com>
2020-10-21 22:45:01 +0100

    First commit
    First commit. Works fully for the specific case required. Needs to be generalised

--- Main.py
+++ Main.py
@@ -4,14 +4,14 @@ import PySimpleGUI as sg
 import win32com.client as w32
 import win32api
 from collections import namedtuple
-from itertools import groupby
+from itertools import groupby, chain
 from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 import reportlab
 from reportlab.lib.units import mm
 from reportlab.pdfgen import canvas
 from operator import attrgetter
 import pandas as pd
-
+import re
 
 
 """
@@ -29,10 +29,10 @@ outlook = w32.Dispatch('Outlook.Application')
 mapi = outlook.GetNamespace('MAPI')
 inbox = mapi.GetDefaultFolder(6)
 mailbox = inbox.Parent
-pdflist =list()
+pdflist = list()
 
 # ------ Some constants ------ #
-savefolder = os.path.join(os.path.expandvars("%userprofile%"), 'Documents')
+save_folder = os.path.join(os.path.expandvars("%userprofile%"), 'Documents')
 firstname = win32api.GetUserNameEx(3)
 firstname = firstname.split()[1]
 firstname = re.findall('^[A-Za-z]+', firstname)[0]
@@ -41,11 +41,21 @@ sg.theme('LightGreen')
 
 # ------ Set up the Tree with the root node as the default mailbox of Outlook ------ #
 treedata = sg.TreeData()
-treedata.Insert(parent='', key=mailbox.Name, text=mailbox.Name, 
+treedata.Insert(parent='', key=mailbox.Name, text=mailbox.Name,
                 values=[mailbox.StoreID, mailbox.EntryID], icon=folder_icon)
 
 # ------ Custom Class to handle the format of the pdf filenames so the right ones can be grouped and merged in the right order ------ #
+
+
 class MyFileName:
+    """
+    The names of pdf files are in the format:
+    {freq} + {Sec | Appendix} + {part} + {region}
+    e.g. "Monthly Sec 1 EMEA"
+
+    This class takes in a filename and determines those named parts of it
+    """
+
     def __init__(self, filename):
         self.filename = filename
         temp = filename.split()
@@ -56,11 +66,12 @@ class MyFileName:
         if self.section:
             self.freq = filename.split('Sec ')[0].strip()
             self.part = filename.split('Sec ')[1].split()[0].strip()
-            self.region = filename.split('Sec ' = self.part + ' ')[1].strip()
+            self.region = filename.split('Sec ' + self.part + ' ')[1].strip()
         elif self.appendix:
             self.freq = filename.split('Appendix ')[0].strip()
             self.part = filename.split('Appendix ')[1].split()[0].strip()
-            self.region = filename.split('Appendix ' = self.part + ' ')[1].strip()
+            self.region = filename.split(
+                'Appendix ' + self.part + ' ')[1].strip()
 
     def __repr__(self):
         return self.filename
@@ -71,17 +82,21 @@ def add_folders(parent, tree=treedata):
     Iterates through the parent mailbox adding all the folders and subfolders
     to the tree
 
-    Params:
+    Parameters:
+    -----------
     parent: a reference to an Outlook Folder item
     tree: a PySimpleGUI treedata object
 
     Returns:
+    --------
     Nothing
     """
     for f in parent.Folders:
-        tree.Insert(parent=parent.Name, key=f.Name, text=f.Name, values=[f.StoreID, f.EntryID], icon=folder_icon)
+        tree.Insert(parent=parent.Name, key=f.Name, text=f.Name,
+                    values=[f.StoreID, f.EntryID], icon=folder_icon)
         # recursive so all folders and subfolders are added
         add_folders(f)
+
 
 # Add all outlook folders to the tree
 add_folders(mailbox)
@@ -89,9 +104,9 @@ add_folders(mailbox)
 
 # ------ Define the layout of the window with the Tree ------ #
 layout = [[sg.Text('Outlook folder browser\nSelect folder with pdfs to merge',
-                   size=(50,2)),
+                   size=(50, 2)),
            sg.Text('Select pdfs to merge',
-                   size=(40,2))],
+                   size=(40, 2))],
           [sg.Tree(data=treedata,
                    headings=['StoreID', 'EntryID'],
                    auto_size_columns=True,
@@ -103,7 +118,7 @@ layout = [[sg.Text('Outlook folder browser\nSelect folder with pdfs to merge',
                    enable_events=True),
            sg.Listbox(values='',
                       enable_events=True,
-                      size=(50,24),
+                      size=(50, 24),
                       key='-FILES-',
                       select_mode=sg.LISTBOX_SELECT_MODE_EXTENDED)],
           [sg.Button('Get pdfs'), sg.Button('Cancel'), sg.Button('Merge pdfs', disabled=True, key='-MERGE-')]]
@@ -141,7 +156,7 @@ def get_pdfs(folder):
                for m in messages if m.Attachments.Count > 0]
 
     # ------ flatten the list of lists and sort by filename ------ #
-    pdflist = list(itertools.chain.from_iterable(pdflist))
+    pdflist = list(chain.from_iterable(pdflist))
     pdflist = sorted(pdflist, key=attrgetter('filename.filename'))
 
     return pdflist
@@ -150,66 +165,300 @@ def get_pdfs(folder):
 # The StoreID of the folder it came from is optional
 
 
-# ------ Event loop to catch selected Outlook folder and update the listbox ------ #
-while True:
-    event, values = window.read()
-    if event in (None, 'Cancel'):
-        window.close()
-        sys.exit()
-    if event is 'Get pdfs':
-        storeID, entryID = TreeDict[values['-TREE-'][0]].values
-        pdf_list = get_pdfs(outlook.GetFolderFromID(entryID, storeID))
-        window['-FILES-'].update(pdf_list)
-        window['-MERGE-'].update(disabled=False)
-    if event is '-MERGE-':
-        print(event, values)
-        break
+def group_pdfs(pdfs):
+    """
+    Given a list of pdf NamedTuples (as defined above), the function will
+    return a list of lists of pdfs grouped by region/frequency/{sec|appendix}
+    Thus, each sublist will be an ordered list of pdfs to be merged into single docs
+
+    Parameters:
+    -----------
+    pdfs: a list of pdf NamedTuples
+    
+    Returns:
+    --------
+    A list of lists of pdfs in page order, each sublist a separate document
+
+    """
+    
+    # Sort the list of filenames by region/freq/appendex
+    pdfs.sort(key=lambda x: [x.filename.region, x.filename.freq, x.filename.appendix])
+    
+    # Get unique pairs of (freq, region)
+    merge_these = {(x.filename.freq, x.filename.region) for x in pdfs}
+    
+    # Split all files into separate lists by freq/region
+    result = [[x for x in pdfs if (x.filename.freq, x.filename.region) == m] for m in merge_these]
+    
+    return result
+
+def send_mail(send_to, send_cc=None, subject='Test', attachment_path=None, send_bcc=None, body_text='Test mail'):
+    """
+    Create an outlook mail and prepare to send it
+    """
+    new_mail = outlook.CreateItem(0x0) # 0x0='olMailItem'
+    new_mail.To = send_to
+    
+    if send_cc:
+        new_mail.CC = send_cc
+    else:
+        new_mail.CC = ''
+        
+    if subject:
+        new_mail.Subject = subject
+    else:
+        new_mail.Subject = ''
+    
+    if send_bcc:
+        new_mail.BCC = send_bcc
+    else:
+        new_mail.BCC = ''
+        
+    if attachment_path:
+        new_mail.Attachments.Add(attachment_path)
+    
+    if body_text:
+        new_mail.Body = body_text
+    else:
+        new_mail.Body = ''
+    
+    new_mail.Display(False)
 
 
 def merge_pdf(pdf_files):
     """
     Iterates through a list of namedtuples of type=pdf(filename, entryID) and merges them into a single file.
     All the files are attachments to Outlook messages stored in a single Outlook folder
-    :param pdf_files: a list of namedtuples of type=pdf
-    :return: a single pdf file saved at the chosen location
+    
+    Parameters:
+    -----------
+    pdf_files: a list of namedtuples of type=pdf
+    
+    Returns:
+    --------
+    The path to a single pdf file saved at the chosen location
     """
-    save_folder = sg.PopupGetFolder('Choose merged pdf file save location')
+    
+    # save_folder = sg.PopupGetFolder('Choose merged pdf file save location')
+    
+    merged_filename = ' '.join([pdf_files[0].filename.freq, pdf_files[0].filename.region])
+    
+    if not merged_filename.endswith('.pdf'):
+        merged_filename += '.pdf'
+        
+    print (f'Working on {merged_filename}')
+    
     merger = PdfFileMerger()
     for f in pdf_files:
         # ------ first create a reference to the message the pdf file is stored in ------ #
-        msg = outlook.GetItemFromID(f.entryID) # storeID is optional
+        msg = mapi.GetItemFromID(f.entryID)  # storeID is optional
         # ------ then get the pdf file ------ #
         pdf_file = msg.Attachments.Item(f.index)
         # ------ save the pdf file and then add it to the merger job ------ #
-        pdf_file.SaveAsFile(os.path.join(save_folder, f.filename))
-        merger.append(fileobj=os.path.join(save_folder, f.filename))
+        pdf_file.SaveAsFile(os.path.join(save_folder, f.filename.filename))
+        merger.append(fileobj=os.path.join(save_folder, f.filename.filename))
+        
     # ------ save the merged file ------ #
-    merger.write(os.path.join(save_folder, 'MergedPdf.pdf'))
+    merger.write(os.path.join(save_folder, merged_filename))
     merger.close()
-    sg.Popup('MergedPdf saved at {}'.format(save_folder))
+    #sg.Popup('MergedPdf saved at {}'.format(save_folder))
+    
     # ------ ask whether to keep the source pdf files or delete them ------ #
     del_choice = sg.PopupYesNo('Delete original pdfs from saved location?')
     if del_choice == 'Yes':
         for f in pdf_files:
             os.remove(os.path.join(save_folder, f.filename))
         sg.Popup('Source pdfs have been deleted')
+        
+    return os.path.join(save_folder, merged_filename)
 
 
+def import_mailing_list(mailing_list=None):
+    """
+    Reads a csv file into a pandas dataframe and returns that dataframe
+    
+    Parameters:
+    -----------
+    mailing_list: the filename and path location of the mailing list.
+        The file headers must be:
+            To:
+            Cc:
+            Bcc:
+            Addressee
+            Report Name
+            Team Name
+    
+    Returns:
+    --------
+    pandas dataframe
+    """
+    
+    # if no path is supplied then ask for a specific file
+    if not mailing_list:
+        email_data = sg.PopupGetFile('Select csv file wth email data', 
+                                     default_path=os.path.join(os.path.expandvars('%userprofile%'),
+                                                               'Documents',
+                                                               'emaildata.csv'))
+    else:
+        email_data = mailing_list
+        
+    result = pd.read_csv(email_data)
+    return result
 
-merge_pdf(values['-FILES-'])
+
+def create_pdf_canvas(source, output):
+    """
+    Creates a blank pdf file with the same number of pages as the source file.
+    Each page will be the same size as the source file and numbered at the 
+    bottom centre of each page
+    
+    The purpose is to add page numbers to files that don't have them
+
+    Args:
+        source (pdf file): a file opened with PdfFileReader
+        output (string): the name of the output file
+    """
+    
+    # Create the base canvas the pdf doc
+    c = canvas.Canvas(output)
+    
+    # Add a page for each page in the source to the base
+    for i in range(source.getNumPages()) :
+        c.setFont('Helvetica', 7)
+        w = source.pages[i].mediaBox[2]
+        h = source.pages[i].mediaBox[3]
+        c.setPageSize((w, h))
+        
+        # add the page number
+        if i > 0:
+            c.drawString(w//2, (4)*mm, str(i)) 
+        c.showPage()
+    c.save()
+    return
 
 
-# print(pdf_dict.values())
-print(pdf_list)
+def combine_merged_canvas(basepdf, pagespdf):
+    """
+    Combines the single pdf file created with merge_pdf() with the blank canvas
+    pdf with page numbers only from create_pdf_canvas
 
-work_folder = outlook.GetFolderFromID(entryID, storeID)
-print('There are {} messages in this folder'.format(work_folder.Items.Count))
+    Args:
+        basepdf (pdf file): merged_pdf filepath and filename
+        pagespdf (pdf file): pdf_canvas filepath and filename
+    
+    Returns:
+        overwrites basepdf
+    """
+    
+    # read the pdf files
+    corePdf = PdfFileReader(basepdf)
+    numberPdf = PdfFileReader(pagespdf)
+    
+    # combine the two pdf files
+    output = PdfFileWriter()
+    for p in range(corePdf.getNumPages()):
+        page = corePdf.getPage(p)
+        numberLayer = numberPdf.getPage(p)
+        page.mergePage(numberLayer)
+        output.addPage(page)
+        
+    with open(basepdf, 'wb') as f:
+        output.write(f)
+    
+    
+def create_merged_pdf_and_mail(pdfs):
+    """
+    Performs all the following actions:
+    1. Groups all pdfs into those that should be joined together
+    2. Merges them into separate files
+    3. Attaches merged file to a mail for specific recipients
+    
+    Parameters:
+    -----------
+        pdfs: a list of NamedTuples of pdfs chosen to go through this process
+    """        
+    
+    # 1. Import the mailing list
+    email_list = import_mailing_list()
+    
+    # 2. Get list of lists of pdfs grouped by freq & region
+    grp_pdfs = group_pdfs(pdfs)
+    
+    # 3. Join pdfs in each group and attach to a new mail
+    for gp in grp_pdfs:
+        print(f'There are {len(gp)} pdf files to merge...')
+        single_pdf = merge_pdf(gp)
+        outputfilename = os.path.join(save_folder, 'pdfpages.pdf')
+        create_pdf_canvas(PdfFileReader(single_pdf), outputfilename)
+        combine_merged_canvas(single_pdf, outputfilename)
+        
+        freq = gp[0].filename.freq
+        reg = gp[0].filename.region
+        
+        if reg.endswith('.pdf'):
+            reg = reg[:-4]
+        
+        mask = ((email_list['Report Name'] == freq) & (email_list['Team Name'] == reg))
+        print(f'Number of filess with corresponding Report Name & Team Name is {mask.sum()}')
+        
+        if mask.sum() == 0:
+            print(f'''There are no combinations of
+                  Report Name = {freq}
+                  Team Name = {reg}\n''')
+        else:
+            email_to = email_list.loc[mask, 'To:'].values[0]
+            email_cc = email_list.loc[mask, 'Cc:'].values[0]
+            email_bcc = email_list.loc[mask, 'Bcc:'].values[0]
+            email_addressee = email_list.loc[mask, 'Addressee'].values[0]
+            
+            print(f'''
+                  Frequency: {freq}
+                  Region: {reg}
+                  To: {email_to})
+                  Cc: {email_cc}
+                  Bcc: {email_bcc}
+                  Addressee: {email_addressee}
+                  ''')
+            
+            mail_text = 'Dear {0}\n\nPlease find attached your {1} MI.\n\nKind Regards,\n\n{2}'.format(
+                email_addressee,
+                freq.split()[0],
+                firstname
+            )
+            
+            send_mail(send_to=email_to,
+                      send_cc=email_cc,
+                      send_bcc=email_bcc,
+                      subject=' '.join([freq, reg]),
+                      attachment_path=single_pdf,
+                      body_text=mail_text)
+            
 
-work_folder_Restricted = work_folder.Items.Restrict("[ReceivedTime] >= '23/12/2019'") #December 23, 2019
-print('There are {} messages in this folder since 23rd Dec 2019'.format(work_folder_Restricted.Count))
-for msg in work_folder_Restricted:
-    print(msg.Subject)
+# ------ Event loop to catch selected Outlook folder and update the listbox ------ #
+while True:
+    event, values = window.read()
+    if event in (None, 'Cancel'):
+        window.close()
+        sys.exit()
+    if event == 'Get pdfs':
+        storeID, entryID = TreeDict[values['-TREE-'][0]].values
+        pdf_list = get_pdfs(mapi.GetFolderFromID(entryID, storeID))
+        window['-FILES-'].update([x.filename.filename for x in pdf_list])
+        window['-MERGE-'].update(disabled=False)
+    if event == '-MERGE-':
+        merging_pdfs = ([x for x in pdf_list if x.filename.filename in values['-FILES-']])
+        create_merged_pdf_and_mail(merging_pdfs)
+        window.close()
+        sys.exit()
 
-window.close()
-sys.exit()
 
+ 
+# work_folder = outlook.GetFolderFromID(entryID, storeID)
+# print('There are {} messages in this folder'.format(work_folder.Items.Count))
+
+# work_folder_Restricted = work_folder.Items.Restrict(
+#     "[ReceivedTime] >= '23/12/2019'")  # December 23, 2019
+# print('There are {} messages in this folder since 23rd Dec 2019'.format(
+#     work_folder_Restricted.Count))
+# for msg in work_folder_Restricted:
+#     print(msg.Subject)
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # PDF Merger and Mailer
- Merge pdf files from selected outlook folder and email them
+ 
+ This script first creates a tree of all your Microsoft Outlook folders. Once
+ one of them is selected it will go through all the mails in that folder and
+ provide a list of all the pdf attachments.
+
+ The purpose of the program was originally to capture individual pdf pages that 
+ were emailed and then collate the pages into a single doc. The pdf page 
+ filenames were formatted in this form:
+
+ {frequency} + {Sec | Appendix} + {part} + {region}
+    e.g. "Monthly Sec 1 EMEA"
+
+ So, continuing from there then all the attachments are merged into single
+ pdf files, grouped by common frequency & region.
+
+ Lastly, the pages were sent on email to specific persons the list of which is 
+ kept in a csv file. The column headers of that file are:
+    To:
+    Cc:
+    Bcc:
+    Addressee
+    Report Name
+    Team Name
+
+This python program required a number of interesting packages:
+    pysimplegui: for creating the outlook folder tree and pop-up messages
+    win32api & win32com.client: for connecting with the Outlook COM interface
+    PyPDF2 & reportlab: for reading, writing and creating pdf files
